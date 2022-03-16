962dbc55c9314800ddad856da7a2fb30871d7cf9
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-03-16 10:42:50 +0530

    Add project files.

---
+++ Week12Day3Demo/Controllers/ParameterTestController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Week12Day3Demo.Models;
+
+namespace Week12Day3Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParameterTestController : ControllerBase
+    {
+        [HttpGet("AddNumbers")]
+        public int Get(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpPost("CheckStudentRollNo")]
+        public bool CheckStudent(Student student)
+        {
+            if (student.RollNo < 100)
+                return true;
+
+            return false;
+        }
+
+
+        [HttpPost("GetStudent")]
+        public Student GetStudent()
+        {
+            return new Student { RollNo = 101, Name = "Raman Gujral" };
+        }
+
+
+        [HttpPost("GetStudents")]
+        public List<Student> GetStudentList()
+        {
+            return new List<Student> {
+                new Student { RollNo = 101, Name = "Raman Gujral" },
+                new Student { RollNo = 101, Name = "Naman Pujmal" }
+            };
+        }
+
+        [HttpPost("GetStudents2")]
+        public IEnumerable<Student> GetStudentIEnumerable()
+        {
+            return new List<Student> {
+                new Student { RollNo = 101, Name = "Raman Gujral" },
+                new Student { RollNo = 101, Name = "Naman Pujmal" }
+            };
+        }
+
+        [HttpGet("GetMeSomething")]
+        public IActionResult GetMeSomething(int value)
+        {
+            if (value < 10)
+                return BadRequest();
+            else if (value > 100)
+                return NoContent();
+
+            return Ok();
+        }
+
+        [HttpGet("GetMeANaughtyStudent")]
+        public ActionResult<Student> GetAStudent(int rollNo)
+        {
+            if (rollNo > 0)
+                return new Student { RollNo = rollNo, Name = "Something Something" };
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Week12Day3Demo/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Week12Day3Demo.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+    }
+}
